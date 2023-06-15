4b815ebf12fc9686f3bcb4f9a1d3605511149ed9
Natali <saromydova-natasha@yandex.ru>
2023-06-15 22:07:55 +0300

    Homework 4 C# Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Enter number: ");
+string inputedString = Console.ReadLine();
+int number = Convert.ToInt32(inputedString);
+
+Console.WriteLine($"In the range from 1 to {number} even numbers: ");
+
+int X=2;
+while(X<= number)
+{
+    Console.WriteLine(X);
+    X +=2;
+}
